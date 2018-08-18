898e3d4c667f20bdb5937f1f6a994122989c32f5
Patricia Carter <patricia4194@gmail.com>
2018-08-18 16:57:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeon_list = {}
+  
+  data[:gender].each do |gender, names|
+    names.each do |name|
+      pigeon_list[name] = {
+        :gender => ["#{gender.to_s}"]
+      }
+    end 
+  end 
+  
+  data.each do |attribute, data_set|
+    
+    data_set.each do |sub_attribute, array|
+      
+      array.each do |name|
+        binding.pry
+      end 
+      
+    end 
+    
+  end 
+  
+  
+  return pigeon_list
 end
