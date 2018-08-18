529fa00ffc0919a841fd8322d1176c902287d996
Patricia Carter <patricia4194@gmail.com>
2018-08-18 17:06:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeon_list = {}
+  
+  data[:gender].each do |gender, names|
+    names.each do |name|
+      pigeon_list[name] = {
+        :gender => ["#{gender.to_s}"]
+      }
+    end 
+  end 
+  
+  data.each do |attribute, data_set|
+    
+    data_set.each do |sub_attribute, array|
+      
+      array.each do |name|
+        if pigeon_list[name].key?(attribute) == false
+          pigeon_list[name][attribute] = [sub_attribute.to_s]
+        else 
+          if attribute != :gender
+            pigeon_list[name][attribute] << sub_attribute.to_s
+          end 
+        end
+      end 
+      
+    end 
+    
+  end 
+  
+  
+  return pigeon_list
 end
